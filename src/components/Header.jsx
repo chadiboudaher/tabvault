de92a70f6a132ad0e90676ceda52e9d9e14bc680
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-17 22:46:48 +0300

    Second BETTER design for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [currentNav, setCurrentNav] = useState("1");
+
+  const navTitles = [
+    {
+      id: "1",
+      title: "Home",
+    },
+    {
+      id: "2",
+      title: "About",
+    },
+    {
+      id: "3",
+      title: "Explore",
+    },
+    {
+      id: "4",
+      title: "Features",
+    },
+  ];
+
+  function handleClickedNav(id) {
+    setCurrentNav(id);
+    console.log(currentNav);
+  }
+
   return (
-    <header className="flex justify-between items-center px-8 py-3 bg-prussian-blue-400 text-alabaster-grey-50">
+    <header className="flex justify-between items-center pl-8 pr-3 py-3 bg-prussian-blue-400 text-alabaster-grey-50 rounded-[50px] my-4 mx-4">
       <h1 className="text-2xl font-bold tracking-wider text-pumpkin-spice-500">
         TabVault
       </h1>
 
-      <nav>
-        <ul className="flex gap-4 text-xm sm:text-xm text-gray-200">
-          <li>
+      <nav className="bg-prussian-blue-300 rounded-full">
+        <ul className="flex items-center gap-4 text-xm sm:text-xm text-gray-200">
+          {navTitles.map((nav) => (
+            <li key={nav.id}>
+              <a
+                href={`#${nav.title.toLowerCase()}`}
+                onClick={() => handleClickedNav(nav.id)}
+                className={`block px-4 py-2 rounded-full transition-all duration-400 ${
+                  currentNav === nav.id
+                    ? "bg-pumpkin-spice-400 text-white"
+                    : "hover:bg-gray-700"
+                }`}
+              >
+                {nav.title}
+              </a>
+            </li>
+          ))}
+          {/* <li>
             <a href="#home">Home</a>
           </li>
           <li>
@@ -20,11 +61,11 @@ const Header = () => {
           </li>
           <li>
             <a href="#features">Features</a>
-          </li>
+          </li> */}
         </ul>
       </nav>
 
-      <button className="py-2 px-3 bg-orange-500 rounded-lg">
+      <button className="py-3 px-4 bg-pumpkin-spice-500 rounded-[40px]">
         Get Started
       </button>
     </header>
