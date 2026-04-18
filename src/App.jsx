e2334dafc33f870dca73924457ec88cd88feb74c
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-18 21:47:50 +0300

    Did some clean up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="h-dvh overflow-x-hidden bg-stormy-teal-100">
+    <div className="h-dvh overflow-x-hidden bg-muted-teal-400">
       <Header />
       <Hero />
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="h-[640px] bg-neutral-950 text-white rounded-[40px] mx-4">
-      <div className="pl-16 pt-16">
+      <div className="absolute pl-16 pt-[6rem]">
         <h1 className="text-5xl md:text-7xl font-bold font-aldrich leading-tight">
           Organize Your <br />
-          <span className="text-neon-chartreuse-400">Learning.</span>
+          <span className="text-seaweed-500">Learning.</span>
         </h1>
 
         <p className="mt-6 text-gray-400 text-lg md:text-xl">
@@ -14,7 +14,7 @@ const Hero = () => {
           and never lose context again.
         </p>
 
-        <button className="mt-10 px-8 py-3 bg-white -neon-chartreuse-400 rounded-[30px] font-medium hover:scale-105 transition-transform">
+        <button className="mt-10 px-8 py-4 text-onyx-900 bg-neon-chartreuse-400 rounded-[30px]">
           Start Organizing
         </button>
       </div>
