333c6152ce7037d2820fc5e4503230757e66a240
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-18 19:15:02 +0300

    Update palette and fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,14 +41,14 @@ const Header = () => {
   return (
     <header
       id="header-tab"
-      className="flex justify-between items-center pl-8 pr-3 py-3 bg-prussian-blue-400 text-alabaster-grey-50 rounded-[50px] my-4 mx-4"
+      className="flex justify-between items-center pl-8 pr-3 py-3 bg-onyx-900 text-seaweed-400 rounded-[50px] my-4 mx-4"
     >
-      <h1 className="text-2xl font-bold tracking-wider text-pumpkin-spice-500">
+      <h1 className="text-2xl font-bold tracking-wider  font-aldrich">
         TabVault
       </h1>
 
-      <nav className="bg-prussian-blue-300 rounded-full">
-        <ul className="flex items-center gap-4 text-xm sm:text-xm text-gray-200">
+      <nav className="rounded-full">
+        <ul className="flex items-center gap-4 text-xm sm:text-xm text-gray-200 font-jetbrains">
           {navTitles.map((nav) => (
             <li key={nav.id}>
               <a
@@ -56,30 +56,18 @@ const Header = () => {
                 onClick={() => handleClickedNav(nav.id)}
                 className={`block px-4 py-2 rounded-full transition-all duration-400 ${
                   currentNav === nav.id
-                    ? "bg-pumpkin-spice-400 text-white"
-                    : "hover:bg-gray-700"
+                    ? "bg-seaweed-500 text-white"
+                    : "hover:bg-seaweed-800"
                 }`}
               >
                 {nav.title}
               </a>
             </li>
           ))}
-          {/* <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#explore">Explore</a>
-          </li>
-          <li>
-            <a href="#features">Features</a>
-          </li> */}
         </ul>
       </nav>
 
-      <button className="py-3 px-4 bg-pumpkin-spice-500 rounded-[40px]">
+      <button className="py-3 px-4 bg-neon-chartreuse-400 text-onyx-800 rounded-[40px]">
         Get Started
       </button>
     </header>
